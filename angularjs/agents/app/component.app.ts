ea68e55634ecc3766b1fab53bdf0b91c22990b3e
Lei YE <wutiandengdai@gmail.com>
2019-03-14 22:26:54 +0800

    Add angular js day1 example.

---
+++ angularjs/agents/app/component.app.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app',
+    // template: `
+    //     <div class = "card search">
+    //     <h1 class="search-headline">Agents List</h1>
+    //     <label class="search-label">Search</label>
+    //     </div>
+    // `
+    templateUrl: 'app/html/app.html'
+})
+
+export class AppComponent{}
